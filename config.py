86c9970502034843cf0f1c2dc7dfa2ff00b5312a
Vladyslav Savustianenko <mwstechnology\vladyslavs>
2021-05-04 11:27:45 +0300

    db config

--- config.py
+++ config.py
@@ -7,7 +7,8 @@ con = psycopg2.connect(
     host="ec2-52-44-31-100.compute-1.amazonaws.com",
     database="d1fqct9sb89al",
     user="egsukzxripuiby",
-    password="26df186754bd4f578f5f684ca7cf855542ecace095f61d02a3d0d9556752ba49"
+    password="26df186754bd4f578f5f684ca7cf855542ecace095f61d02a3d0d9556752ba49",
+    port="5432"
 )
 cur = con.cursor()
 target = 'https://chat-server-lab3.herokuapp.com/'
