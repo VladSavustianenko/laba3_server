de9646b7b3f506f555bfa8b8795cf646213274fc
Vladyslav Savustianenko <mwstechnology\vladyslavs>
2021-05-04 10:56:21 +0300

    credentials

--- config.py
+++ config.py
@@ -4,13 +4,13 @@ from flask_cors import CORS
 
 
 con = psycopg2.connect(
-    database="laba3",
-    user="postgres",
-    password="admin"
+    host="ec2-52-44-31-100.compute-1.amazonaws.com",
+    database="d1fqct9sb89al",
+    user="egsukzxripuiby",
+    password="26df186754bd4f578f5f684ca7cf855542ecace095f61d02a3d0d9556752ba49"
 )
 cur = con.cursor()
-new_db_name = "laba3"
-target = 'http://localhost:5000'
+target = 'https://chat-server-lab3.herokuapp.com/'
 
 app = Flask(__name__)
 CORS(app)
