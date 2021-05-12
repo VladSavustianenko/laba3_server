291e3a450bc61b38880b7dea7a01d0803d28628e
Vladyslav Savustianenko <vladsavustianenko@gmail.com>
2021-05-12 10:44:06 +0300

    config getenv

--- config.py
+++ config.py
@@ -3,13 +3,13 @@ from flask import Flask
 from flask_cors import CORS
 import os
 
-
+print(os.getenv("HOST"))
 con = psycopg2.connect(
-    host=os.environ.get("HOST"),
-    database=os.environ.get("DATABASE"),
-    user=os.environ.get("USER"),
-    password=os.environ.get("PASSWORD"),
-    port=os.environ.get("PORT")
+    host=os.getenv("HOST"),
+    database=os.getenv("DATABASE"),
+    user=os.getenv("USER"),
+    password=os.getenv("PASSWORD"),
+    port=os.getenv("PORT")
 )
 cur = con.cursor()
 target = 'https://chat-server-lab3.herokuapp.com/'
