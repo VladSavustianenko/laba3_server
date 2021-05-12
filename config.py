7dc8baba700018c9db951e59de5f1cb12c3e2be6
Vladyslav Savustianenko <vladsavustianenko@gmail.com>
2021-05-12 11:25:52 +0300

    config revert2

--- config.py
+++ config.py
@@ -3,18 +3,18 @@ from flask import Flask
 from flask_cors import CORS
 import os
 
-print(os.environ['HOST'])
+
 con = psycopg2.connect(
-    # host=os.environ['HOST'],
-    # database=os.environ['DATABASE'],
-    # user=os.environ['USER'],
-    # password=os.environ['PASSWORD'],
-    # port=os.environ['PORT']
-    host='ec2-52-44-31-100.compute-1.amazonaws.com',
-    database='d1fqct9sb89al',
-    user='egsukzxripuiby',
-    password='26df186754bd4f578f5f684ca7cf855542ecace095f61d02a3d0d9556752ba49',
-    port=5432
+    host=os.environ['HOST'],
+    database=os.environ['DATABASE'],
+    user=os.environ['USER'],
+    password=os.environ['PASSWORD'],
+    port=os.environ['PORT']
+    # host='ec2-52-44-31-100.compute-1.amazonaws.com',
+    # database='d1fqct9sb89al',
+    # user='egsukzxripuiby',
+    # password='26df186754bd4f578f5f684ca7cf855542ecace095f61d02a3d0d9556752ba49',
+    # port=5432
 )
 cur = con.cursor()
 target = 'https://chat-server-lab3.herokuapp.com/'
