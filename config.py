63313652b1d3c3b82258718454d4d36d28ac43e9
Vladyslav Savustianenko <vladsavustianenko@gmail.com>
2021-05-12 10:18:52 +0300

    config update

--- config.py
+++ config.py
@@ -1,14 +1,15 @@
 import psycopg2
 from flask import Flask
 from flask_cors import CORS
+import os
 
 
 con = psycopg2.connect(
-    host="ec2-52-44-31-100.compute-1.amazonaws.com",
-    database="d1fqct9sb89al",
-    user="egsukzxripuiby",
-    password="26df186754bd4f578f5f684ca7cf855542ecace095f61d02a3d0d9556752ba49",
-    port="5432"
+    host=os.environ.get("HOST"),
+    database=os.environ.get("DATABASE"),
+    user=os.environ.get("USER"),
+    password=os.environ.get("PASSWORD"),
+    port=os.environ.get("PORT")
 )
 cur = con.cursor()
 target = 'https://chat-server-lab3.herokuapp.com/'
