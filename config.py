eee0bba2934969ee09a20a1935485020501f44b6
Vladyslav Savustianenko <vladsavustianenko@gmail.com>
2021-05-12 12:11:20 +0300

    add proxy.py

--- config.py
+++ config.py
@@ -1,23 +1,18 @@
 import psycopg2
 from flask import Flask
 from flask_cors import CORS
-import os
+import proxy
 
 
 con = psycopg2.connect(
-    host=os.environ.get('HOST'),
-    database=os.environ.get('DATABASE'),
-    user=os.environ.get('USER'),
-    password=os.environ.get('PASSWORD'),
-    port=os.environ.get('PORT')
-    # host='ec2-52-44-31-100.compute-1.amazonaws.com',
-    # database='d1fqct9sb89al',
-    # user='egsukzxripuiby',
-    # password='26df186754bd4f578f5f684ca7cf855542ecace095f61d02a3d0d9556752ba49',
-    # port=5432
+    host=proxy.HOST,
+    database=proxy.DATABASE,
+    user=proxy.USER,
+    password=proxy.PASSWORD,
+    port=proxy.PORT
 )
 cur = con.cursor()
-target = 'https://chat-server-lab3.herokuapp.com/'
+target = proxy.TARGET
 
 app = Flask(__name__)
 CORS(app)
--- proxy.py
+++ proxy.py
@@ -3,3 +3,4 @@ DATABASE='d1fqct9sb89al'
 USER='egsukzxripuiby'
 PASSWORD='26df186754bd4f578f5f684ca7cf855542ecace095f61d02a3d0d9556752ba49'
 PORT=5432
+TARGET='https://chat-server-lab3.herokuapp.com/'
