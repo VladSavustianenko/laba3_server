4a4b8dac9409d0fddfd9957fc98b0331dd973069
Vladyslav Savustianenko <vladsavustianenko@gmail.com>
2021-05-19 17:55:02 +0300

    use sqlalchemy

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import psycopg2
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 import proxy
 
@@ -14,7 +15,11 @@ cur = con.cursor()
 target = proxy.TARGET
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = f'postgresql://{proxy.USER}:{proxy.PASSWORD}@{proxy.HOST}/{proxy.DATABASE}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
 CORS(app)
+
+db = SQLAlchemy(app)
 
 
 def close_connection():
--- create_db.py
+++ create_db.py
@@ -2,102 +2,39 @@ import config
 
 cur = config.cur
 con = config.con
+db = config.db
 
 
-def create_user_table():
-    try:
-        cur.execute(
-            f"CREATE TABLE Person "
-            f"("
-            f"id integer NOT NULL,"
-            f"email character varying NOT NULL,"
-            f"password character varying NOT NULL,"
-            f"CONSTRAINT user_pkey PRIMARY KEY (id)"
-            f")"
-        )
-        con.commit()
-        print("Table User created!")
-    except:
-        con.commit()
-        print("Table User already exist!")
+class Person(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String, nullable=True)
+    password = db.Column(db.String, nullable=True)
 
 
-def create_profile_table():
-    try:
-        cur.execute(
-            f"CREATE TABLE Profile "
-            f"("
-            f"id integer NOT NULL,"
-            f"name character varying NOT NULL,"
-            f"surname character varying NOT NULL,"
-            f"birth date,"
-            f"CONSTRAINT profile_pkey PRIMARY KEY (id)"
-            f")"
-        )
-        con.commit()
-        print("Table Profile created!")
-    except:
-        con.commit()
-        print("Table Profile already exist!")
+class Profile(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=True)
+    surname = db.Column(db.String, nullable=True)
+    birth = db.Column(db.String)
 
 
-def create_avatar_table():
-    try:
-        cur.execute(
-            f"CREATE TABLE Avatar "
-            f"("
-            f"id integer NOT NULL,"
-            f"url character varying, "
-            f"CONSTRAINT avatar_pkey PRIMARY KEY (id)"
-            f")"
-        )
-        con.commit()
-        print("Table Avatar created!")
-    except:
-        con.commit()
-        print("Table Avatar already exist!")
+class Avatar(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    url = db.Column(db.String)
 
 
-def create_message_table():
-    try:
-        cur.execute(
-            f"CREATE TABLE Message "
-            f"("
-            f"id integer NOT NULL,"
-            f"user_id integer NOT NULL,"
-            f"text character varying NOT NULL,"
-            f"date character varying,"
-            f"time character varying,"
-            f"CONSTRAINT message_pkey PRIMARY KEY (id)"
-            f")"
-        )
-        con.commit()
-        print("Table Message created!")
-    except:
-        con.commit()
-        print("Table Message already exist!")
+class Message(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=True)
+    text = db.Column(db.String, nullable=True)
+    date = db.Column(db.String)
+    time = db.Column(db.String)
 
 
-def create_session_table():
-    try:
-        cur.execute(
-            f"CREATE TABLE Session "
-            f"("
-            f"id integer NOT NULL,"
-            f"token character varying NOT NULL,"
-            f"CONSTRAINT session_pkey PRIMARY KEY (token)"
-            f")"
-        )
-        con.commit()
-        print("Table Session created!")
-    except:
-        con.commit()
-        print("Table Session already exist!")
+class Session(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    token = db.Column(db.String, nullable=True)
 
 
 def create():
-    create_user_table()
-    create_profile_table()
-    create_avatar_table()
-    create_message_table()
-    create_session_table()
+    db.create_all()
